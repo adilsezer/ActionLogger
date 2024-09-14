4ead800ff4b0284d1ff4ddaa20a511e2413186e8
Adil Sezer <sezeradil@gmail.com>
2024-09-14 01:31:03 +0100

    Fixing administrator issues

--- ActionLogger/Views/App.xaml.cs
+++ ActionLogger/Views/App.xaml.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ActionLogger.Views;
+using System.Security.Principal;
 using System.Windows;
 
 namespace ActionLogger
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Check if the application is running as administrator
+            if (!IsRunningAsAdministrator())
+            {
+                // Show a message box to inform the user that admin privileges are required
+                MessageBox.Show("The application requires administrator privileges to run correctly. Please restart the application as an administrator.", "Access Denied", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                // Exit the application gracefully
+                Application.Current.Shutdown();
+                return;
+            }
+
+            // Now that we are sure admin privileges are present, manually create and show MainWindow
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        /// <summary>
+        /// Determines if the current application is running with administrator privileges.
+        /// </summary>
+        /// <returns>True if the app is running as an administrator, otherwise false.</returns>
+        private bool IsRunningAsAdministrator()
+        {
+            using (var identity = WindowsIdentity.GetCurrent())
+            {
+                var principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
     }
 }
